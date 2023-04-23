0a63a6ab8d468cc9b02f7aae755c1a1c91d5091f
Karla <karla.gardiner@gmail.com>
2023-04-23 17:06:24 +0100

    Added dynamodb table for vocab and retrieved with lambda

--- src/handlers/retrieve-vocab-list.ts
+++ src/handlers/retrieve-vocab-list.ts
@@ -1,4 +1,5 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { DynamoDBClient, QueryCommand } from '@aws-sdk/client-dynamodb';
 import { Logger } from '@aws-lambda-powertools/logger';
 
 /**
@@ -12,23 +13,32 @@ import { Logger } from '@aws-lambda-powertools/logger';
  */
 
 const logger = new Logger();
+const dbClient = new DynamoDBClient({ region: 'eu-west-2' });
 
 export const handle = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    logger.info('in lambda handler');
-    try {
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: 'hello world',
-            }),
-        };
-    } catch (err) {
-        console.log(err);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({
-                message: 'some error happened',
-            }),
-        };
-    }
+  logger.info('in lambda handler');
+  const tableName = process.env['VOCAB_TABLE_NAME'];
+  logger.info('table', { tableName });
+  const params = {
+    TableName: tableName,
+    KeyConditionExpression: 'userId = :id',
+    ExpressionAttributeValues: { ':id': { S: 'user123' } },
+  };
+  logger.info('params', { params });
+  try {
+    const { Items } = await dbClient.send(new QueryCommand(params));
+    logger.info('users vocab:', { Items });
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ Items }),
+    };
+  } catch (error) {
+    logger.error('Failed to retrieve vocab list', { error });
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'Vocab items not retrieved',
+      }),
+    };
+  }
 };
